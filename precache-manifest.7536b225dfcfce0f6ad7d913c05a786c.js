1af7be20113d9432b1d8fcca2812cdd757025b0d
RoshRoslin <RoshRoslin@gmail.com>
2021-05-07 18:36:03 -0700

    Updates

---
+++ precache-manifest.7536b225dfcfce0f6ad7d913c05a786c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26b395a89c87c78b03d0039ca3216046",
+    "url": "/react-accordion-project/index.html"
+  },
+  {
+    "revision": "75f096f80ff6c2ebd7b9",
+    "url": "/react-accordion-project/static/css/main.4b589e59.chunk.css"
+  },
+  {
+    "revision": "ab85a117f39d501e282e",
+    "url": "/react-accordion-project/static/js/2.40b1d22a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-accordion-project/static/js/2.40b1d22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75f096f80ff6c2ebd7b9",
+    "url": "/react-accordion-project/static/js/main.caa8a3ef.chunk.js"
+  },
+  {
+    "revision": "688b9b0ef66409eb5eac",
+    "url": "/react-accordion-project/static/js/runtime-main.8aae537a.js"
+  }
+]);
