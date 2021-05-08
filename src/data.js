60048f111c6635b3c9ff3c3d1f3122cac92babd2
RoshRoslin <RoshRoslin@gmail.com>
2021-05-07 18:36:33 -0700

    Commit source code to master branch

---
+++ src/data.js
@@ -0,0 +1,33 @@
+const questions = [
+  {
+    id: 1,
+    title: 'Do I have to allow the use of cookies?',
+    info:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit fugiat ipsum facere asperiores alias repudiandae cupiditate esse, quaerat omnis necessitatibus?',
+  },
+  {
+    id: 2,
+    title: 'How do I change my My Page password?',
+    info:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni fuga porro iure facilis harum eius cumque voluptas doloribus, neque placeat!',
+  },
+  {
+    id: 3,
+    title: 'What is BankID?',
+    info:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis et minima harum necessitatibus quis? Omnis ea minima similique illo sequi exercitationem doloremque beatae! Voluptatem at vel, quaerat voluptatibus consequatur iure?',
+  },
+  {
+    id: 4,
+    title: 'Whose birth number can I use?',
+    info:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste temporibus modi nobis nisi numquam dolores voluptate quis, laboriosam qui dolorum cupiditate, commodi atque quas consequatur fugit debitis perferendis ad quidem?',
+  },
+  {
+    id: 5,
+    title: 'When do I recieve a password ordered by letter?',
+    info:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto tempore doloribus quisquam eveniet aspernatur ipsum autem ut? Odio, ullam doloribus nihil aliquam inventore impedit ad iusto ducimus facilis eum?',
+  },
+];
+export default questions;
